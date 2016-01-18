c71a98ae6bccb38ac356351a33357ea089506a5d
Shlomi Assaf <shlomiasaf@gmail.com>
2016-01-18 20:04:25 +0200

    webpack dev hack

---
+++ src/components/angular2-modal/angular2-modal.ts
@@ -0,0 +1,9 @@
+export * from './models/ICustomModal';
+export * from './models/ModalConfig';
+export * from './models/ModalDialogInstance';
+export * from './components/modalBackdrop';
+export * from './components/bootstrapModalContainer';
+export * from './providers/Modal';
+
+export * from './commonModals/yesNoModal';
+export * from './commonModals/okOnlyModal';
