42348ede832bdaaaba0fe3335b4c4c549b390fb8
Shlomi Assaf <shlomiasaf@gmail.com>
2016-03-05 21:37:09 +0200

    run webpack on es5 code (commonjs)

---
+++ webpack.config.test.js
@@ -0,0 +1,77 @@
+var path = require('path');
+var webpack = require('webpack');
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+
+module.exports = {
+    // for faster builds use 'eval'
+    devtool: 'source-map',
+    debug: true, // remove in production
+
+    entry: {
+        'vendor': './src/demo/vendor.ts',
+        'app': './src/demo/bootstrap.ts' // our angular app
+    },
+
+    // Config for our build files
+    output: {
+        path: path.resolve('./build'),
+        filename: '[name].js',
+        sourceMapFilename: '[name].map',
+        chunkFilename: '[id].chunk.js'
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.ts$/,
+                loader: 'tslint-loader',
+                exclude: [/node_modules/] }
+        ],
+        loaders: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                query: {
+                    'configFileName': 'tsconfig.test.json',
+                    'ignoreDiagnostics': [
+                        2403, // 2403 -> Subsequent variable declarations
+                        2300, // 2300 -> Duplicate identifier
+                        2374, // 2374 -> Duplicate number index signature
+                        2375  // 2375 -> Duplicate string index signature
+                    ]
+                },
+                exclude: [ /\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/ ]
+            },
+            { test: /\.json$/,  loader: 'json-loader' },
+            { test: /\.css$/,   loader: 'raw-loader' },
+            { test: /\.html$/,  loader: 'raw-loader' },
+
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/font-woff" },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&minetype=application/font-woff" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=application/octet-stream" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=image/svg+xml" }
+
+        ],
+        noParse: [ /.+zone\.js\/dist\/.+/, /.+angular2\/bundles\/.+/ ]
+    },
+    plugins: [
+        new CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js', minChunks: Infinity }),
+        new CommonsChunkPlugin({ name: 'common', filename: 'common.js', minChunks: 2, chunks: ['app', 'vendor'] })
+    ],
+    resolve: {
+        root: [
+            path.resolve('./src/demo'),
+            path.resolve('./dist')
+        ],
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.json', '.css', '.html']
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: '',
+        publicPath: '/build'
+    },
+    tslint: {
+        emitErrors: false,
+        failOnHint: false
+    },
+};
