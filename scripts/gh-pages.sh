7e1f343bec0cf7ddaa3f3f786e77faf323aba4bb
Shlomi Assaf (shlassaf) <shlassaf@cisco.com>
2016-06-14 18:05:32 +0300

    fix: github publishing

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,16 +1,5 @@
-originBranch=$(git symbolic-ref --short -q HEAD)
-
 npm run build
-git checkout gh-pages
 
-branch=$(git symbolic-ref --short -q HEAD)
-if [ "$branch" = "gh-pages" ]; then
-    cp -R ./dist/build/* ./
-    git add .
-    git commit -m "gh-pages update"
-    git push origin gh-pages
-else
-    echo Error: could not checkout gh-pages
-fi
-
-git checkout $originBranch
+rm ./dist/build/index.html
+cp -R ./src/index.ghpages.html ./dist/build/index.html
+gh-pages -d dist/build
