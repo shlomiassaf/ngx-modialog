d1d26f5e879c2495cf4291f028ceb13a3f2e38a8
Jeremy Fensch <JeremyF@mvpsi.com>
2016-07-01 13:41:21 -0400

    rc.4 changes

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,7 +1,6 @@
 import { ComponentRef, PLATFORM_DIRECTIVES } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import { BROWSER_APP_PROVIDERS } from '@angular/platform-browser';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import { MODAL_BROWSER_PROVIDERS } from './components/angular2-modal/platform-browser';
@@ -23,7 +22,6 @@ export function main(): Promise<ComponentRef<App>> {
       {provide: PLATFORM_DIRECTIVES, multi: true, useValue: ROUTER_DIRECTIVES},
       APP_ROUTER_PROVIDERS,
       {provide: LocationStrategy, useClass: HashLocationStrategy},
-      BROWSER_APP_PROVIDERS // remove in production
     ])
       .catch(err => console.error(err));
   }
