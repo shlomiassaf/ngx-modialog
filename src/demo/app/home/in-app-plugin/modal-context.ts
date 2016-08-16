15e5d7dddd5d31ffc74d94a98507aefbaff266ab
Shlomi Assaf (shlassaf) <shlassaf@cisco.com>
2016-08-16 16:42:53 +0300

    misc: rebase commit

---
+++ src/demo/app/home/in-app-plugin/modal-context.ts
@@ -0,0 +1,34 @@
+import { TemplateRef } from '@angular/core';
+import {
+  Modal,
+  ModalOpenContextBuilder,
+  ModalOpenContext,
+  FluentAssignMethod
+} from '../../../../components/angular2-modal';
+
+
+export class InAppModalContext extends ModalOpenContext {
+
+  title: string;
+  templateRef: TemplateRef<any>;
+  normalize(): void {
+    if (!this.message) this.message = '';
+  }
+}
+
+
+export class InAppModalContextBuilder extends ModalOpenContextBuilder<InAppModalContext> {
+  title: FluentAssignMethod<string, this>;
+  templateRef: FluentAssignMethod<TemplateRef<any>, this>;
+
+  constructor(
+    modal: Modal
+  ) {
+    super(
+      <any>{ modal },
+      ['title', 'templateRef'],
+      <any>InAppModalContext
+      // https://github.com/Microsoft/TypeScript/issues/7234
+    );
+  }
+}
