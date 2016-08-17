5d268dff850a7c240ea10567ea7055cabe912659
isaacplmann <isaacplmann@users.noreply.github.com>
2016-08-17 14:43:15 -0400

    Update bootstrap.module.ts

--- src/components/angular2-modal/plugins/bootstrap/bootstrap.module.ts
+++ src/components/angular2-modal/plugins/bootstrap/bootstrap.module.ts
@@ -15,7 +15,7 @@ import {
   Modal as BaseModal,
   ModalBackdropComponent,
   ModalDropInFactory
-} from '../../../../components/angular2-modal';
+} from '../../../angular2-modal';
 
 function getProviders(): any[] {
   return [
