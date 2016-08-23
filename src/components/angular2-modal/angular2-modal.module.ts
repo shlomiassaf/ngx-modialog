250f2348f65153cdd736ea1a7d665ba6ebd40129
Shlomi Assaf (shlassaf) <shlassaf@cisco.com>
2016-08-23 13:19:30 +0300

    feat: allow registering dynamic components

--- src/components/angular2-modal/angular2-modal.module.ts
+++ src/components/angular2-modal/angular2-modal.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { ANALYZE_FOR_ENTRY_COMPONENTS, NgModule, ModuleWithProviders, Type } from '@angular/core';
 import { EVENT_MANAGER_PLUGINS } from '@angular/platform-browser';
 
 import { DOMOutsideEventPlugin, DOMOverlayRenderer } from './providers/index';
@@ -27,13 +27,35 @@ import { Overlay, ModalOverlay, OverlayDialogBoundary, OverlayTarget } from './o
   ]
 })
 export class ModalModule {
-  static forRoot(): ModuleWithProviders {
+  /**
+   * Returns a ModalModule pre-loaded with a list of dynamically inserted components.
+   * Since dynamic components are not analysed by the angular compiler they must register manually
+   * using entryComponents, this is an easy way to do it.
+   * @param entryComponents A list of dynamically inserted components (dialog's).
+   * @returns {{ngModule: ModalModule, providers: {provide: OpaqueToken, useValue: Array<Type|any[]>, multi: boolean}[]}}
+   */
+  static withComponents(entryComponents: Array<Type | any[]>): ModuleWithProviders {
+    return {
+      ngModule: ModalModule,
+      providers : [
+        { provide: ANALYZE_FOR_ENTRY_COMPONENTS, useValue: entryComponents, multi: true }
+      ]
+    }
+  }
+
+  /**
+   * Returns a NgModule for use in the root Module.
+   * @param entryComponents A list of dynamically inserted components (dialog's).
+   * @returns ModuleWithProviders
+   */
+  static forRoot(entryComponents?: Array<Type | any[]>): ModuleWithProviders {
     return {
       ngModule: ModalModule,
       providers: [
         Overlay,
         { provide: OverlayRenderer, useClass: DOMOverlayRenderer },
         { provide: EVENT_MANAGER_PLUGINS, useClass: DOMOutsideEventPlugin, multi: true },
+        { provide: ANALYZE_FOR_ENTRY_COMPONENTS, useValue: entryComponents || [], multi: true }
       ]
     };
   }
